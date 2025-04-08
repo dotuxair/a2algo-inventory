8414d2db7bb57289e2e313d17812afa747539aa9
Uzair <dotuxair@gmail.com>
2025-04-08 12:02:20 +0500

    routing for sales , products and purchases

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,13 @@
 import { Routes } from '@angular/router';
+import { ProductsComponent } from './components/products/products.component';
+import { SalesComponent } from './components/sales/sales.component';
+import { PurchaseComponent } from './components/purchase/purchase.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    { path: '', redirectTo: '/products', pathMatch: 'full' },
+    { path: 'products', component:ProductsComponent, },
+    { path: 'sales', component:SalesComponent },
+    { path: 'purchases', component:PurchaseComponent },
+
+
+];
